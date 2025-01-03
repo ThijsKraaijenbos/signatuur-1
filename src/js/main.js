929412b7c0e23e6496badf13144a3ba5d278d8b7
Thijs Kraaijenbos <47988622+ThijsKraaijenbos@users.noreply.github.com>
2025-01-03 23:32:29 +0100

    Add basic animation for text section headers (using GSAP library)

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,91 @@
-import { gsap } from "gsap/all";
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/all";
-import { ScrollSmoother } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 window.addEventListener("load", () => init());
 
 const init = () => {
     console.log("test")
-    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-    let smoother = ScrollSmoother.create({
-        wrapper: '#smooth-wrapper',
-        content: '#smooth-content',
-    })
+    gsap.fromTo(".onderzoeken-header h2", {
+        y: 20,
+        opacity: 0,
+    },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 1.5,
+            ease: "power3.inOut",
+            scrollTrigger: {
+                trigger: ".onderzoeken-header h2",
+                scrub: 3,
+                end: "top center",
+                endTrigger: ".onderzoeken-text",
+                once: true,
+                markers: true,
+            }
+        }
+    );
+
+    gsap.fromTo("#onderzoeken-gradient-bar", {
+        opacity: 0,
+        x: -200,
+    },
+        {
+            x: 0,
+            opacity: 1,
+            duration: 1.5,
+            ease: "power3.inOut",
+            scrollTrigger: {
+                trigger: "#onderzoeken-gradient-bar",
+                scrub: 3,
+                end: "top center",
+                endTrigger: ".onderzoeken-text",
+                once: true,
+                markers: true,
+            }
+        }
+    );
+
+
+
+    gsap.fromTo(".ontwerpen-header h2", {
+        y: 20,
+        opacity: 0,
+    },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 1.5,
+            ease: "power3.inOut",
+            scrollTrigger: {
+                trigger: ".ontwerpen-header h2",
+                scrub: 3,
+                end: "top center",
+                endTrigger: ".ontwerpen-text",
+                once: true,
+            }
+        }
+    );
+
+    gsap.fromTo("#ontwerpen-gradient-bar", {
+        opacity: 0,
+        x: -200,
+    },
+        {
+            x: 0,
+            opacity: 1,
+            duration: 1.5,
+            ease: "power3.inOut",
+            scrollTrigger: {
+                trigger: "#ontwerpen-gradient-bar",
+                scrub: 3,
+                end: "top center",
+                endTrigger: ".ontwerpen-text",
+                once: true,
+            }
+        }
+    );
 
 }
